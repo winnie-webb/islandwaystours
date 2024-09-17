5cd396060ec0307a4acc2fa95ebbd90ab32d7255
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-09-17 12:41:04 -0500

    Remove dummy image from home

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 import Image from "next/image";
-import logo from "../public/airport-transfer/at-1.webp";
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-50">
-      <Image src={logo} width={500} height={500} alt="Logo" />
-    </div>
+    <div className="flex justify-center items-center h-screen bg-gray-50"></div>
   );
 }

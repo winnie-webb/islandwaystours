29a296f4b828e291845f29f2b641f67a1cf83328
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-09-18 18:42:22 -0500

    Added Gallery Section

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import Hero from "./components/Hero";
+import Gallery from "./components/Gallery";
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Gallery />
+    </>
+  );
 }

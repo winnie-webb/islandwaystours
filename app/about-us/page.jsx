a1713a06862d068550bccb2b2036f9935d30379f
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-10-07 21:13:54 -0500

    Links to previous commit

---
+++ app/about-us/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutUs from "../components/AboutUs";
+
+function page() {
+  return (
+    <div>
+      <AboutUs></AboutUs>
+    </div>
+  );
+}
+
+export default page;
